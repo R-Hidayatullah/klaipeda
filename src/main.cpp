c74870808a696088f3bbf9cb712aeea45356f783
Ridwan Hidayatullah <24.ridwanhidayatullah.9b@gmail.com>
2025-12-05 18:10:24 +0700

    fresh start

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,57 @@
-#include "application.h"
+#include <iostream>
+#include <tinyxml2.h>
+using namespace tinyxml2;
+
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+void testFreetype()
+{
+    FT_Library ft;
+    if (FT_Init_FreeType(&ft))
+    {
+        printf("Freetype init FAILED!\n");
+        return;
+    }
+    printf("Freetype init OK!\n");
+
+    // Try loading any .ttf
+    if (FT_New_Face(ft, "C:/Windows/Fonts/arial.ttf", 0, nullptr))
+    {
+        printf("Load font FAILED\n");
+    }
+    else
+    {
+        printf("Font load OK\n");
+    }
+
+    FT_Done_FreeType(ft);
+}
+
+void testTinyXML2()
+{
+    const char *xml =
+        "<root>"
+        "  <child name='abc'>123</child>"
+        "</root>";
+
+    XMLDocument doc;
+    if (doc.Parse(xml) == XML_SUCCESS)
+    {
+        printf("TinyXML2 OK\n");
+        auto *child = doc.FirstChildElement("root")->FirstChildElement("child");
+        printf("child text = %s\n", child->GetText());
+    }
+    else
+    {
+        printf("TinyXML2 FAILED!\n");
+    }
+}
 
 int main()
 {
-    Application app{};
-    app.ipf_root.file_path = "C:\\Users\\Ridwan Hidayatullah\\Documents\\TreeOfSaviorCN\\data\\animation.ipf";
-    parse_ipf(app.ipf_root);
-    if (!initialize(app))
-        return -1;
-    run(app);
-    cleanup(app);
+    std::cout << "Hello World!\n";
+    testFreetype();
+    testTinyXML2();
     return 0;
 }
